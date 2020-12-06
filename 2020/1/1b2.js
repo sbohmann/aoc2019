34daae4053c4909ce900040f4b35d61e30a3e62c
Sebastian Bohmann <mail@sebastian-bohmann.com>
2020-12-06 11:00:04 +0100

    16b2: Group object

--- 2020/1/1b2.js
+++ 2020/1/1b2.js
@@ -5,7 +5,7 @@ function solve() {
     // let input = [1721, 979, 366, 299, 675, 1456]
     let input = readInput()
     let result
-    forEachGroup(3, input, group => {
+    Group(3, input).forEach(group => {
         let sum = group.reduce((lhs, rhs) => lhs + rhs)
         if (sum === 2020) {
             if (result) {
@@ -24,24 +24,28 @@ function readInput() {
         .map(line => Number(line.trim()))
 }
 
-function forEachGroup(groupSize, data, action) {
-    forEachSubGroup(groupSize, 0, data, action)
-}
-
-function forEachSubGroup(groupSize, startIndex, data, action) {
-    if (groupSize === 1) {
-        forEachSingleElementGroup(startIndex, data, action)
-    } else {
-        for (let index = startIndex; index < data.length; ++index) {
-            forEachSubGroup(groupSize - 1, index + 1, data,
-                subGroup => action([data[index]].concat(subGroup)))
+function Group(groupSize, data) {
+    function forEachSubGroup(subGroupSize, startIndex, action) {
+        if (subGroupSize === 1) {
+            forEachSingleElementGroup(startIndex, action)
+        } else {
+            for (let index = startIndex; index < data.length; ++index) {
+                forEachSubGroup(subGroupSize - 1, index + 1,
+                    subGroup => action([data[index]].concat(subGroup)))
+            }
         }
     }
-}
 
-function forEachSingleElementGroup(startIndex, data, action) {
-    for (let index = startIndex; index < data.length; ++index) {
-        action([data[index]])
+    function forEachSingleElementGroup(startIndex, action) {
+        for (let index = startIndex; index < data.length; ++index) {
+            action([data[index]])
+        }
+    }
+
+    return {
+        forEach(action) {
+            forEachSubGroup(groupSize, 0, action)
+        }
     }
 }
 
