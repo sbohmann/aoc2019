b7792fba1fd1a79f537a7e246c79b004227f63ed
Sebastian Bohmann <mail@sebastian-bohmann.com>
2020-12-26 10:20:07 +0100

    2020 / 7a extracted Relation

--- 2020/7/7.js
+++ 2020/7/7.js
@@ -1,7 +1,5 @@
 import {readLines} from '../../common/io.js'
-
-const containerPattern = () => /(\w+ \w+) bags contain (no other bags)?/
-const containedPattern = () => /(\d+) (\w+ \w+) (bags?)/
+import Relation from './relation.js'
 
 function solveA() {
     let relations = readLines('input.txt')
@@ -12,85 +10,6 @@ function solveA() {
             relation.contained.forEach(contained => console.log(contained.number, contained.color))
             console.log()
         })
-}
-
-function Relation(line) {
-    let containerColor
-    let contained
-
-    function read() {
-        let containerMatch = matchContainerPart()
-        containerColor = containerMatch[1]
-
-        let noOtherBags = containerMatch[2] != null
-        if (noOtherBags) {
-            contained = []
-        } else {
-            let containedListStart = containerMatch[0].length
-            contained = readContainedBags(containedListStart)
-        }
-    }
-
-    function matchContainerPart() {
-        let result = line.match(containerPattern())
-        if (result === null) {
-            throw new Error('Illegal input line [' + line + ']')
-        }
-        return result
-    }
-
-    function readContainedBags(containedListStart) {
-        let containedInput = extractContainedInput(containedListStart)
-        return readContainedColors(containedInput)
-    }
-
-    function extractContainedInput(containedListStart) {
-        if (line[line.length - 1] !== '.') {
-            throw new Error('line does not end in dot [' + line + ']')
-        }
-        let containedListLength = line.length - containedListStart - 1
-        return line.substr(containedListStart, containedListLength)
-    }
-
-    function readContainedColors(input) {
-        return input
-            .split(', ')
-            .map(readContainedColor)
-    }
-
-    function readContainedColor(input) {
-        let match = matchContainedPart(input)
-        let number = Number(match[1])
-        let color = match[2]
-        checkContainedDeclension(number, match[3], input, match)
-        return {
-            number,
-            color
-        }
-    }
-
-    function matchContainedPart(input) {
-        let result = input.match(containedPattern())
-        if (result === null) {
-            throw new Error('Illegal contained part [' + input + ']')
-        }
-        return result
-    }
-
-    function checkContainedDeclension(number, declension, input, match) {
-        let numberIsOne = number === 1
-        let declensionIsSingular = declension === 'bag'
-        if (numberIsOne !== declensionIsSingular) {
-            throw new Error('Illegal declension [' + declension + '] for number ' + number + ' in line [' + line + '], contained input [' + input + '], match [' + match + ']')
-        }
-    }
-
-    read()
-
-    return {
-        color: containerColor,
-        contained
-    }
 }
 
 solveA()
