b97ccf452ad0c52b8a6e476a531d3285a7ce7d4e
sbohmann <mail@sebastian-bohmann.com>
2022-12-02 08:32:05 +0100

    2022 1b - still thinking about how to nicely update the top 3 array

--- 2022/1/1.js
+++ 2022/1/1.js
@@ -6,8 +6,10 @@ let parts = input.split(/\n\n/)
 
 function partSum(part) {
     let items = part.split(/\n/).map(Number)
-    return items.reduce((a, b) => a + b)
+    return items.reduce(add)
 }
+
+let add = (a, b) => a + b
 
 let max = (a, b) => Math.max(a, b)
 
@@ -18,7 +20,30 @@ let maximum = partSums.reduce(max)
 console.log(maximum)
 
 function updateTopThree(state, value) {
-    // 🤔
+    for (let index = 0; index < state.length; ++index) {
+        if (value > state[index]) {
+            return insert(state, index, value)
+        }
+    }
+    if (state.length < 3) {
+        let result = [...state]
+        result.push(value)
+        return result
+    }
+    return state
+}
+
+function insert(state, position, value) {
+    result = state.slice(0, position)
+    result.push(value)
+    let n = 3 - result.length
+    for (let offset = 0; offset < n; ++offset) {
+        result.push(state[position + offset])
+    }
+    return result
 }
 
 let topThree = partSums.reduce(updateTopThree, [])
+
+console.log(topThree)
+console.log(topThree.reduce(add))
