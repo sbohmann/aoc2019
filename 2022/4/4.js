7d71c8dbcc030ba93187a25d79c3697fa12f8f94
sbohmann <mail@sebastian-bohmann.com>
2022-12-06 11:03:01 +0100

    2022 4 completed

--- 2022/4/4.js
+++ 2022/4/4.js
@@ -8,24 +8,53 @@ let result4a =
         .map(lineResult)
         .reduce(add)
 
+console.log("4a:", result4a)
+
+let result4b =
+    fs.readFileSync('input.txt', 'utf-8')
+        .split('\n')
+        .filter(line => line.length > 0)
+        .map(lineRanges)
+        .filter(lineRangesOverlap)
+        .length
+
+console.log("4b:", result4b)
+
 function lineResult(line) {
-    let match = line.match(/^(\d+)-(\d+),(\d+)-(\d+)$/)
-    let firstRange = range(match[1], match[2])
-    let secondRange = range(match[3], match[4])
-    if (firstRange.contains(secondRange) || secondRange.contains(firstRange)) {
+    let {first, second} = lineRanges(line)
+    if (first.containsRange(second) || second.containsRange(first)) {
         return 1
     }
     return 0
+}
+
+function lineRanges(line) {
+    let match = line.match(/^(\d+)-(\d+),(\d+)-(\d+)$/)
+    let first = range(match[1], match[2])
+    let second = range(match[3], match[4])
+    return {first, second}
 }
 
 function range(first, last) {
     return {
         first: Number(first),
         last: Number(last),
-        contains(other) {
+        contains(value) {
+            return value >= first && value <= last
+        },
+        containsRange(other) {
             return other.first >= first && other.last <= last
         }
     }
 }
 
-console.log("4a:", result4a)
+function lineRangesOverlap(lineRanges) {
+    return rangesOverlap(lineRanges.first, lineRanges.second)
+}
+
+function rangesOverlap(a, b) {
+    return a.contains(b.first) ||
+        a.contains(b.second) ||
+        b.contains(a.first) ||
+        b.contains(a.second)
+}
