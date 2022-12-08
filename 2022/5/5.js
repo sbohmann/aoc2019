4849ef93498a486326e0d280cce753ce39e7fb01
sbohmann <mail@sebastian-bohmann.com>
2022-12-08 11:10:03 +0100

    2022 5a (reading map, no transformation to columns yet)

--- 2022/5/5.js
+++ 2022/5/5.js
@@ -8,16 +8,31 @@ if (input.length !== 2) {
     throw new RangeError
 }
 
-let map = input[0].split('\n')
-let mapHeight = map.length - 1
-let columnsLineMatch = map[mapHeight].match(/( *\d+ *)/)
-if (!columnsLineMatch) {
-    throw new RangeError()
-}
-let columnMatches = map[mapHeight].matchAll(/\d+/)
-let numberOfColumns = 0
-for (let match of columnMatches) {
-    ++numberOfColumns
-}
+let mapLines = input[0].split('\n')
+let mapHeight = mapLines.length - 1
+let columnsLine = mapLines[mapHeight]
+let columnMatches = columnsLine.matchAll(/ *(\d+)/g)
+let numberOfColumns = [...columnMatches].length
 
 console.log(numberOfColumns)
+
+let map = []
+for (let maoLineIndex = 0; maoLineIndex < mapHeight; ++maoLineIndex) {
+    const line = mapLines[maoLineIndex]
+    const columnsInLine = (line.length + 1) / 4
+    let row = []
+    for (let columnIndex = 0; columnIndex < columnsInLine; ++columnIndex) {
+        let columnStart = columnIndex * 4
+        let columnMatch = line
+            .substring(columnStart, columnStart + 3)
+            .match(/\[([A-Z])]/)
+        if (columnMatch) {
+            row.push(columnMatch[1])
+        } else {
+            row.push(null)
+        }
+    }
+    map.push(row)
+}
+
+console.log(map)
