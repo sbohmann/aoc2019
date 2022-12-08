70bf4ad1b526b3a0dbb8dded3d07280d16f5a9a5
sbohmann <mail@sebastian-bohmann.com>
2022-12-08 18:54:15 +0100

    2022 5a - parsing column stacks

--- 2022/5/5.js
+++ 2022/5/5.js
@@ -9,39 +9,45 @@ if (input.length !== 2) {
     throw new RangeError
 }
 
-let mapLines = input[0].split('\n')
-let mapHeight = mapLines.length - 1
-let columnsLine = mapLines[mapHeight]
-let columnMatches = columnsLine.matchAll(/ *(\d+)/g)
-let numberOfColumns = [...columnMatches].length
-
-console.log(numberOfColumns)
-
-let map = []
-for (let mapLineIndex = 0; mapLineIndex < mapHeight; ++mapLineIndex) {
-    const line = mapLines[mapLineIndex]
-    const columnsInLine = (line.length + 1) / 4
-    let row = []
-    for (let columnIndex = 0; columnIndex < columnsInLine; ++columnIndex) {
-        let columnStart = columnIndex * 4
-        let columnMatch = line
-            .substring(columnStart, columnStart + 3)
-            .match(/\[([A-Z])]/)
-        if (columnMatch) {
-            row.push(columnMatch[1])
-        } else {
-            row.push(null)
-        }
-    }
-    map.push(row)
-}
-
-console.log(map)
-
 let table = TabularText(input[0], 4)
 console.log(table)
 for (let y = 0; y < table.height; ++y) {
     for (let x = 0; x < table.width; ++x) {
         console.log(`[${table.get(x, y)}]`)
     }
+}
+
+let stacks = readStacks()
+console.log(stacks)
+
+function readStacks() {
+    let result = new Map()
+    let columnNumberRow = table.height - 1
+    for (let x = 0; x < table.width; ++x) {
+        let columnNumber = Number(table.get(x, columnNumberRow).trim())
+        let stack = parseColumn(x)
+        result.set(columnNumber, stack)
+    }
+    return result
+}
+
+function parseColumn(x) {
+    let stack = []
+    let foundEmptyCell = false
+    for (let y = table.height - 2; y >= 0; --y) {
+        let cellValue = table.get(x, y)
+        let cellValueMatch = cellValue.match(/\[([A-Z])]/)
+        if (cellValueMatch) {
+            if (foundEmptyCell) {
+                throw new RangeError(`Unexpected continuation of column at (${x},${y})`)
+            }
+            stack.push(cellValueMatch[1])
+        } else {
+            if (cellValue.trim() !== '') {
+                throw new RangeError(`Unrecognized non-empty cell value [${cellValue}] at column at (${x},${y})`)
+            }
+            foundEmptyCell = true
+        }
+    }
+    return stack
 }
